e6b02c9edb46fe711f2fa05fa53b695432671ebf
Cloudwu8888 <cloudwu8888@hotmail.com>
2017-07-28 16:27:15 +0800

    AddPush

---
+++ xamarin/xamarin/FirstViewController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Azure.Mobile.Analytics;
+
+using UIKit;
+
+namespace xamarin
+{
+    public partial class FirstViewController : UIViewController
+    {
+        protected FirstViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            Analytics.TrackEvent("The First View");
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ xamarin/xamarin/SecondViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Azure.Mobile.Analytics;
+
+using UIKit;
+
+namespace xamarin
+{
+    public partial class SecondViewController : UIViewController
+    {
+        protected SecondViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            Analytics.TrackEvent("The second view");
+            throw new SystemException("");
+
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
